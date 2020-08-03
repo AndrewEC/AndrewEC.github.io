314a9014d8fab45749823aee57342e8885060844
unknown <andrew.e.cumming@gmail.com>
2020-08-03 00:57:56 -0400

    Added table of contents, back to top footer.

--- static/scripts/portfolio.js
+++ static/scripts/portfolio.js
@@ -3,7 +3,7 @@
     var buttonId = 'reveal-email';
     var spanId = 'email-container';
 
-    var doWithElement = function(id, callback){
+    var doWithElement = function(id, callback) {
         var element = document.getElementById(id);
         if(!element){
             console.error('Could not find element with id: ' + id);
@@ -12,28 +12,28 @@
         callback(element);
     };
 
-    var compose = function(id, callback){
+    var compose = function(id, callback) {
         return function(){
             doWithElement(id, callback);
         }
     };
 
-    var hideButton = compose(buttonId, function(button){
+    var hideButton = compose(buttonId, function(button) {
         button.style.display = 'none';
     });
 
-    var revealEmail = compose(spanId, function(span){
+    var revealEmail = compose(spanId, function(span) {
         var email = 'YW5kcmV3LmUuY3VtbWluZ0BnbWFpbC5jb20=';
         try{
             email = atob(email);
         }catch(e){
             return console.error('Could not decode email address.');
         }
-        span.innerHTML = email;
         hideButton();
+        span.innerHTML = email;
     });
 
-    window.onload = compose(buttonId, function(button){
+    window.onload = compose(buttonId, function(button) {
         button.addEventListener('click', revealEmail, true);
     });
     
